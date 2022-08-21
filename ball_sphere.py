bbf3c23711697168a641a66192340a32e17081e1
mohbarati <mohbarati14@gmail.com>
2022-08-21 10:13:31 +0430

    general cleaning and commenting

--- ball_sphere.py
+++ ball_sphere.py
@@ -1,7 +1,8 @@
+from math import atan, copysign, cos, sin, sqrt
+
+import matplotlib.pyplot as plt
 import pygame
 from pygame.locals import *
-from math import cos, sin, sqrt, atan, copysign
-import matplotlib.pyplot as plt
 
 pygame.init()
 sc = pygame.display.set_mode((650, 600))
@@ -15,10 +16,30 @@ font = pygame.font.SysFont(None, 70)
 
 
 def distance(x, y):
-    return sqrt(x**2 + y**2)
+    """Euclidean distance
+
+    Args:
+        x (float): x-coordinate
+        y (float): y-coordinate
+
+    Returns:
+        float: distance from the center of the big sphere
+    """
+    return sqrt(x ** 2 + y ** 2)
 
 
 def Surface_touch(x, y, r, rsph):
+    """Check if the balls are in contact to calculate Reaction of the surface
+
+    Args:
+        x (float): x-coordinate
+        y (float): y-coordinate
+        r (float): radius of the small ball
+        rsph (float): radius of the big sphere
+
+    Returns:
+        bool: returns True if the balls are in contact
+    """
     if int(distance(x, y) - r) <= rsph:
         return True
     else:
@@ -26,6 +47,16 @@ def Surface_touch(x, y, r, rsph):
 
 
 def draw_circle(c, x, y, r):
+    """draws circle at the given location. because y increases as we go down,
+    so I had to reverse it. also, all shapes are shifted to +200,+300 after
+    calcs are carried out.
+
+    Args:
+        c (tuple): color
+        x (float): x-coordinate
+        y (float): y-coordinate
+        r (float): radius
+    """
     pygame.draw.circle(sc, c, (x + 200, -y + 300), r)
 
 
@@ -47,11 +78,13 @@ def draw_line(color, x_sph, y_sph, r_sph, theta):
     )
 
 
+dt = 0.001  # time interval
+# specs of the big sphere
 x_sph = 0
 y_sph = 0
 r_sph = 200
-# -----------------
-theta = 0.001
+# specs of the ball
+theta = 0.001  # angle of the ball center with respect to y axis
 r_ball = 20
 x = (r_sph + r_ball) * sin(theta)
 y = (r_sph + r_ball) * cos(theta)
@@ -61,9 +94,12 @@ ax, ay = 0, 0
 m = 1
 g = 9.8
 c_drag = 0
+# Drawing the shapes
 drawGrid()
 draw_circle(yellow, x_sph, y_sph, r_sph)
 draw_circle(white, x, y, r_ball + 1.32)
+pygame.display.update()
+# collecting data for the graphs
 T = []
 X = []
 Y = []
@@ -73,12 +109,12 @@ AX = []
 AY = []
 counter = 0
 # ---------------
-pygame.display.update()
+# start the loop
 cont = True
-dt = 0.001
 while cont:
-    if 300 - y > 600 - r_ball:
+    if 300 - y > 600 - r_ball:  # when moved out of the frame (only y)
         cont = False
+    # graph data
     counter += 1
     T.append(counter * dt)
     X.append(x)
@@ -92,21 +128,24 @@ while cont:
     draw_circle(yellow, x_sph, y_sph, r_sph)
     draw_line(blue, x_sph, y_sph, r_sph, 0)
     draw_circle(white, x, y, r_ball + 1)
+    # theta = * rad on the screen
     if distance(x, y) < (r_ball + r_sph + 1.315):
         theta_final = theta
         draw_line(blue, x_sph, y_sph, r_sph, theta)
-        img = font.render(f"Angle = {round(theta,2)} rad", True, red)
+        img = font.render(f"Angle = {round(theta,3)} rad", True, red)
         sc.blit(img, (50, 510))
     else:
         draw_line(blue, x_sph, y_sph, r_sph, theta_final)
-        img = font.render(f"Angle = {round(theta_final,2)} rad", True, red)
+        img = font.render(f"Angle = {round(theta_final,3)} rad", True, red)
         sc.blit(img, (50, 510))
+    # model for reaction of the surface, needs to improve
     if Surface_touch(x, y, r_ball, r_sph):
         Rx = m * g * sin(theta) - c_drag * copysign(1, vx) * abs(vx)
         Ry = m * g * cos(theta) - c_drag * copysign(1, vy) * abs(vy)
     else:
         Rx = 0
         Ry = 0
+
     ax = Rx
     ay = Ry - m * g
     vx += ax * dt
@@ -117,6 +156,7 @@ while cont:
 
     pygame.display.update()
     sc.fill(black)
+
     for event in pygame.event.get():
         if event.type == KEYDOWN:
             if event.key == pygame.K_ESCAPE or event.key == K_q:
@@ -125,7 +165,7 @@ while cont:
             cont = False
     sc.fill(black)
 
-pygame.quit()
+
 print("Theta at the point of seperation of the balls: ", theta_final)
 # -----------------------
 # Visualizing the data points
@@ -145,3 +185,4 @@ ax4.set_xlabel("time (s)")
 ax4.set_ylabel("ay      ", rotation=0)
 ax4.grid("true")
 plt.show()
+pygame.quit()
